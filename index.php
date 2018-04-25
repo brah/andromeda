bdd94c9e58837fe85c02266f2c2a6b38f4d63860
dylan <24949824+mythical@users.noreply.github.com>
2018-04-25 22:12:33 +1000

    edit site

--- index.php
+++ index.php
@@ -5,48 +5,55 @@
        exit();
    }
    ?>
+<!DOCTYPE html>
+
+<!--
+this website is open source
+https://github.com/mythical/andromeda/
+bad code but don't steal without credit
+-->
+
 <html>
-   <head>
-      <meta charset="utf-16">
-      <link href="https://i.imgur.com/jcILMnV.png" rel="icon" type="image/ico">
-      <link href="media/styles.css" rel="stylesheet">
-      <link href="media/animate.css" rel="stylesheet">
-      <title>andromeda</title>
-   </head>
-   <body>
-      <div class="center">
-         <div class="animated fadeInDown">
-		    <a href="https://twitter.com/">twitter</a>
-            <xxx>&nbsp;//&nbsp;</xxx>
-            <a href="https://instagram.com/">instagram</a>
-            <xxx>&nbsp;//&nbsp;</xxx>
-            <a href="https://kik.me/atlantis">kik</a>
-            &nbsp;//&nbsp;
-            <a href="https://steamcommunity.com/profiles/76561198405760708">steam</a>
-            &nbsp;//&nbsp;
-			<a href="https://soundcloud.com/mythical">soundcloud</a>
-			<xxx>&nbsp;//&nbsp;</xxx>
-            <a href="https://www.youtube.com/channel/UCROOu8LGfue6600x6CBCt8Q?v=">youtube</a><br>
-            <a href="#"><img src="media/andromeda.png"></a>
-         </div>
-         <div class="animated fadeInUp">
-            <a href="https://tytto.pw/">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;tyttö.pw</a>
-            <xxx>&nbsp;&nbsp;//&nbsp;</xxx>
-            <a href="https://discord.gg">andromeda#0545</a>
-			<xxx>&nbsp;&nbsp;//&nbsp;</xxx>
-			<a href="https://satirical.site/">satirical.site</a><br>			
-         </div>
-      </div>
-      <!-- 
-         Audio
-         Song: I Miss You
-         Artist: Ta-ku
-         -->
-      <audio autoplay="" id="audio" loop="">
-         <source src="https://tytto.pw/media/a.mp3" type="audio/mp3">
-         <script>
-            audio.volume = 0.3;
-         </script>
-      </audio>
-   </body>
+<head>
+	<meta charset="utf-16">
+	<link href="https://i.imgur.com/jcILMnV.png" rel="icon" type="image/ico">
+	<link href="media/styles.css" rel="stylesheet">
+	<link href="media/animate.css" rel="stylesheet">
+	<title>andromeda</title>
+</head>
+
+<body>
+	<script type="text/javascript">
+	   var imgCount = 4;
+	   var dir = 'media/';
+	   var random_count = Math.round(Math.random() * (imgCount - 1)) + 1;
+	   var images = new Array
+	           images[1] = "img1.gif",
+	           images[2] = "img2.gif",
+	           images[3] = "img3.gif",
+	           images[4] = "img4.gif",
+	   document.body.style.backgroundImage = "url(" + dir + images[random_count] + ")";
+	</script>
+
+	<div class="center">
+		<a href="#"><img src="media/andromeda.png"></a>
+
+		<div class="centersms">
+			<a href="https://twitter.com/" target="_blank">twitter/</a>
+			<a href="https://instagram.com/" target="_blank">instagram/</a>
+			<a href="https://kik.me/atlantis" target="_blank">kik.me/atlantis</a>
+			<a href="https://steamcommunity.com/profiles/76561198405760708" target="_blank">steamcommunity.com/id/culture</a>
+			<a href="https://soundcloud.com/mythical" target="_blank">soundcloud.com/mythical</a>
+			<a href="https://www.youtube.com/channel/UCROOu8LGfue6600x6CBCt8Q?v=" target="_blank">youtube.com/..</a>
+		</div>
+	</div>
+	<script>
+	   var audio = new Audio('media/music/ihateyoubutiloveyou.mp3');
+	   audio.preload = true;
+	   audio.autoplay = true;
+	   audio.loop = true;
+	   audio.volume = 0.85;
+	   audio.play();
+	</script>
+</body>
 </html>
